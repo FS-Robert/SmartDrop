7e22824b7f6e1fc3c056f5d30ba289691f85f36a
BruneMejia <madeline.mej07@gmail.com>
2026-09-17 22:27:07 -0600

    problemas de roberto

--- SmartDrop/SmartDrop/urls.py
+++ SmartDrop/SmartDrop/urls.py
@@ -14,11 +14,18 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from pathlib import Path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('App.urls')),
     path('', include('App.api.urls')),
 ]
+
+if settings.DEBUG:
+    static_dir = Path(settings.BASE_DIR) / 'App' / 'Static'
+    urlpatterns += static(settings.STATIC_URL, document_root=str(static_dir))
